9d23e1c7ecda60475f53534d9c1d3dd7719e496a
RomiBettiol <romiibettiol@gmail.com>
2022-01-10 12:37:44 -0300

    commit -m Ultimo commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function NavBar (){
+    return(
+        <>
+            <div>
+                <ul>
+                    <li>INICIO</li>
+                    <li>NOSOTROS</li>
+                    <li>PRODUCTOS</li>
+                    <li>USUARIO</li>
+                </ul>
+            </div>
+        </>
+    )
+}
